e2b1be943b78c589717fb5ad7035e792a12c6f4b
Andrew Morgan <andrew@amorgan.xyz>
2022-04-04 00:12:39 +0100

    pull in new bot code

--- sgapplace.user.js
+++ sgapplace.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         SgapPlace Bot
 // @namespace    https://github.com/anoadragon453/Bot
-// @version      15
+// @version      20
 // @description  The bot for sgappers
-// @author       Squarific
+// @author       David SGaP
 // @match        https://www.reddit.com/r/place/*
 // @match        https://new.reddit.com/r/place/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
@@ -15,67 +15,90 @@
 // @grant        GM_addStyle
 // ==/UserScript==
 
-// Sorry for the messy code, rush and clean don't always go together ;)
-
 var socket;
-var hasOrders = false;
+var order = undefined;
 var accessToken;
 var currentOrderCanvas = document.createElement('canvas');
 var currentOrderCtx = currentOrderCanvas.getContext('2d');
 var currentPlaceCanvas = document.createElement('canvas');
 
+// Global constants
+const DEFAULT_TOAST_DURATION_MS = 10000;
+
 const COLOR_MAPPINGS = {
+    '#6D001A': 0,
     '#BE0039': 1,
     '#FF4500': 2,
     '#FFA800': 3,
     '#FFD635': 4,
+    '#FFF8B8': 5,
     '#00A368': 6,
     '#00CC78': 7,
     '#7EED56': 8,
     '#00756F': 9,
     '#009EAA': 10,
+    '#00CCC0': 11,
     '#2450A4': 12,
     '#3690EA': 13,
     '#51E9F4': 14,
     '#493AC1': 15,
     '#6A5CFF': 16,
+    '#94B3FF': 17,
     '#811E9F': 18,
     '#B44AC0': 19,
+    '#E4ABFF': 20,
+    '#DE107F': 21,
     '#FF3881': 22,
     '#FF99AA': 23,
     '#6D482F': 24,
     '#9C6926': 25,
+    '#FFB470': 26,
     '#000000': 27,
+    '#515252': 28,
     '#898D90': 29,
     '#D4D7D9': 30,
     '#FFFFFF': 31
 };
 
-var order = [];
-for (var i = 0; i < 200000; i++) {
-    order.push(i);
-}
-order.sort(() => Math.random() - 0.5);
+let getRealWork = rgbaOrder => {
+    let order = [];
+    for (var i = 0; i < 4000000; i++) {
+        if (rgbaOrder[(i * 4) + 3] !== 0) {
+            order.push(i);
+        }
+    }
+    return order;
+};
+
+let getPendingWork = (work, rgbaOrder, rgbaCanvas) => {
+    let pendingWork = [];
+    for (const i of work) {
+        if (rgbaOrderToHex(i, rgbaOrder) !== rgbaOrderToHex(i, rgbaCanvas)) {
+            pendingWork.push(i);
+        }
+    }
+    return pendingWork;
+};
 
 (async function () {
     GM_addStyle(GM_getResourceText('TOASTIFY_CSS'));
     currentOrderCanvas.width = 2000;
-    currentOrderCanvas.height = 1000;
+    currentOrderCanvas.height = 2000;
     currentOrderCanvas.style.display = 'none';
     currentOrderCanvas = document.body.appendChild(currentOrderCanvas);
     currentPlaceCanvas.width = 2000;
-    currentPlaceCanvas.height = 1000;
+    currentPlaceCanvas.height = 2000;
     currentPlaceCanvas.style.display = 'none';
     currentPlaceCanvas = document.body.appendChild(currentPlaceCanvas);
 
     Toastify({
-        text: 'Trying to get Access token...',
-        duration: 10000
+        text: 'Getting access token...',
+        duration: DEFAULT_TOAST_DURATION_MS
     }).showToast();
     accessToken = await getAccessToken();
     Toastify({
-        text: 'Got Access token!',
-        duration: 10000
+        text: 'Access token found!',
+        duration: DEFAULT_TOAST_DURATION_MS
     }).showToast();
 
     connectSocket();
@@ -84,22 +107,26 @@ order.sort(() => Math.random() - 0.5);
     setInterval(() => {
         if (socket) socket.send(JSON.stringify({ type: 'ping' }));
     }, 5000);
+    setInterval(async () => {
+        accessToken = await getAccessToken();
+    }, 30 * 60 * 1000)
 })();
 
 function connectSocket() {
     Toastify({
-        text: 'Connecting to SgapPlace server...',
-        duration: 10000
+        text: 'Connecting to David SGaP himself!',
+        duration: DEFAULT_TOAST_DURATION_MS
     }).showToast();
 
     socket = new WebSocket('wss://sgapplace.chat.horse/api/ws');
 
     socket.onopen = function () {
         Toastify({
-            text: 'Connected to SgapPlace server!',
-            duration: 10000
+            text: 'Connected to David\'s mind! Downloading SGaP data...',
+            duration: DEFAULT_TOAST_DURATION_MS
         }).showToast();
         socket.send(JSON.stringify({ type: 'getmap' }));
+        socket.send(JSON.stringify({ type: 'brand', brand: 'userscriptV20' }));
     };
 
     socket.onmessage = async function (message) {
@@ -113,11 +140,22 @@ function connectSocket() {
         switch (data.type.toLowerCase()) {
             case 'map':
                 Toastify({
-                    text: `Loading new map (reason: ${data.reason ? data.reason : 'connected with server'})`,
-                    duration: 10000
+                    text: `Nieuwe map laden (reden: ${data.reason ? data.reason : 'first time map download'})...`,
+                    duration: DEFAULT_TOAST_DURATION_MS
                 }).showToast();
-                currentOrderCtx = await getCanvasFromUrl(`https://sgapplace.chat.horse/maps/${data.data}`, currentOrderCanvas);
-                hasOrders = true;
+                currentOrderCtx = await getCanvasFromUrl(`https://sgapplace.chat.horse/maps/${data.data}`, currentOrderCanvas, 0, 0, true);
+                order = getRealWork(currentOrderCtx.getImageData(0, 0, 2000, 2000).data);
+                Toastify({
+                    text: `Nieuwe map geladen, ${order.length} pixels in totaal`,
+                    duration: DEFAULT_TOAST_DURATION_MS
+                }).showToast();
+                break;
+            case 'toast':
+                Toastify({
+                    text: `Bericht van server: ${data.message}`,
+                    duration: data.duration || DEFAULT_TOAST_DURATION_MS,
+                    style: data.style || {}
+                }).showToast();
                 break;
             default:
                 break;
@@ -126,8 +164,8 @@ function connectSocket() {
 
     socket.onclose = function (e) {
         Toastify({
-            text: `Connection with SgapPlace server got terminated: ${e.reason}`,
-            duration: 10000
+            text: `Disconnected from David. Reason: ${e.reason}`,
+            duration: DEFAULT_TOAST_DURATION_MS
         }).showToast();
         console.error('Socketfout: ', e.reason);
         socket.close();
@@ -136,85 +174,85 @@ function connectSocket() {
 }
 
 async function attemptPlace() {
-    if (!hasOrders) {
-        setTimeout(attemptPlace, 2000); // Trying again in 2sec.
+    if (order === undefined) {
+        setTimeout(attemptPlace, 2000); // probeer opnieuw in 2sec.
         return;
     }
     var ctx;
     try {
-        ctx = await getCanvasFromUrl(await getCurrentImageUrl('0'), currentPlaceCanvas, 0, 0);
-        ctx = await getCanvasFromUrl(await getCurrentImageUrl('1'), currentPlaceCanvas, 1000, 0)
+        ctx = await getCanvasFromUrl(await getCurrentImageUrl('0'), currentPlaceCanvas, 0, 0, false);
+        ctx = await getCanvasFromUrl(await getCurrentImageUrl('1'), currentPlaceCanvas, 1000, 0, false)
+        ctx = await getCanvasFromUrl(await getCurrentImageUrl('2'), currentPlaceCanvas, 0, 1000, false)
+        ctx = await getCanvasFromUrl(await getCurrentImageUrl('3'), currentPlaceCanvas, 1000, 1000, false)
     } catch (e) {
         console.warn('Error retrieving map: ', e);
         Toastify({
             text: 'Error retrieving map. Trying again in 10 sec...',
-            duration: 10000
+            duration: DEFAULT_TOAST_DURATION_MS
         }).showToast();
-        setTimeout(attemptPlace, 10000); // Trying again in 10sec.
+        setTimeout(attemptPlace, 10000); // probeer opnieuw in 10sec.
         return;
     }
 
-    const rgbaOrder = currentOrderCtx.getImageData(0, 0, 2000, 1000).data;
-    const rgbaCanvas = ctx.getImageData(0, 0, 2000, 1000).data;
+    const rgbaOrder = currentOrderCtx.getImageData(0, 0, 2000, 2000).data;
+    const rgbaCanvas = ctx.getImageData(0, 0, 2000, 2000).data;
+    const work = getPendingWork(order, rgbaOrder, rgbaCanvas);
 
-    for (const j of order) {
-        for (var l = 0; l < 10; l++) {
-            const i = (j * 10) + l;
-            // ignore empty order pixels.
-            if (rgbaOrder[(i * 4) + 3] === 0) continue;
-
-            const hex = rgbToHex(rgbaOrder[(i * 4)], rgbaOrder[(i * 4) + 1], rgbaOrder[(i * 4) + 2]);
-            // This pixel is correct
-            if (hex === rgbToHex(rgbaCanvas[(i * 4)], rgbaCanvas[(i * 4) + 1], rgbaCanvas[(i * 4) + 2])) continue;
-
-            const x = i % 2000;
-            const y = Math.floor(i / 2000);
-            Toastify({
-                text: `Trying to place pixel at ${x}, ${y}...`,
-                duration: 10000
-            }).showToast();
-
-            const res = await place(x, y, COLOR_MAPPINGS[hex]);
-            const data = await res.json();
-            try {
-                if (data.errors) {
-                    const error = data.errors[0];
-                    const nextPixel = error.extensions.nextAvailablePixelTs + 3000;
-                    const nextPixelDate = new Date(nextPixel);
-                    const delay = nextPixelDate.getTime() - Date.now();
-                    Toastify({
-                        text: `Pixel got placed too early! Next pixel will be placed at ${nextPixelDate.toLocaleTimeString()}.`,
-                        duration: delay
-                    }).showToast();
-                    setTimeout(attemptPlace, delay);
-                } else {
-                    const nextPixel = data.data.act.data[0].data.nextAvailablePixelTimestamp + 3000;
-                    const nextPixelDate = new Date(nextPixel);
-                    const delay = nextPixelDate.getTime() - Date.now();
-                    Toastify({
-                        text: `Pixel posted at ${x}, ${y}! Next pixel will be placed at ${nextPixelDate.toLocaleTimeString()}.`,
-                        duration: delay
-                    }).showToast();
-                    setTimeout(attemptPlace, delay);
-                }
-            } catch (e) {
-                console.warn('Analyzing response error', e);
-                Toastify({
-                    text: `Analyzing response error: ${e}.`,
-                    duration: 10000
-                }).showToast();
-                setTimeout(attemptPlace, 10000);
-            }
-
-            return;
-        }
+    if (work.length === 0) {
+        Toastify({
+            text: `All pixels are in their place! Trying again in 25 sec...`,
+            duration: 25000
+        }).showToast();
+        setTimeout(attemptPlace, 25000); // probeer opnieuw in 30sec.
+        return;
     }
 
+    const percentComplete = 100 - Math.ceil(work.length * 100 / order.length);
+    const workRemaining = work.length;
+    const idx = Math.floor(Math.random() * work.length);
+    const i = work[idx];
+    const x = i % 2000;
+    const y = Math.floor(i / 2000);
+    const hex = rgbaOrderToHex(i, rgbaOrder);
+
     Toastify({
-        text: `All pixels are at the right place! Trying again in 30 sec...`,
-        duration: 30000
+        text: `Trying to place pixel on ${x}, ${y}... (${percentComplete}% complete, ${workRemaining} left)`,
+        duration: DEFAULT_TOAST_DURATION_MS
     }).showToast();
-    setTimeout(attemptPlace, 30000); // Trying again in 30sec.
+
+    const res = await place(x, y, COLOR_MAPPINGS[hex]);
+    const data = await res.json();
+    try {
+        if (data.errors) {
+            const error = data.errors[0];
+            const nextPixel = error.extensions.nextAvailablePixelTs + 3000;
+            const nextPixelDate = new Date(nextPixel);
+            const delay = nextPixelDate.getTime() - Date.now();
+            const toast_duration = delay > 0 ? delay : DEFAULT_TOAST_DURATION_MS;
+            Toastify({
+                text: `Pixel placed too early! Will try again at ${nextPixelDate.toLocaleTimeString()}.`,
+                duration: toast_duration
+            }).showToast();
+            setTimeout(attemptPlace, delay);
+        } else {
+            const nextPixel = data.data.act.data[0].data.nextAvailablePixelTimestamp + 3000;
+            const nextPixelDate = new Date(nextPixel);
+            const delay = nextPixelDate.getTime() - Date.now();
+            const toast_duration = delay > 0 ? delay : DEFAULT_TOAST_DURATION_MS;
+            Toastify({
+                text: `Pixel placed at ${x}, ${y}! Will place next pixel at ${nextPixelDate.toLocaleTimeString()}.`,
+                duration: toast_duration
+            }).showToast();
+            setTimeout(attemptPlace, delay);
+        }
+    } catch (e) {
+        console.warn('Analysing error', e);
+        Toastify({
+            text: `Received an error when placing: ${e}.`,
+            duration: DEFAULT_TOAST_DURATION_MS
+        }).showToast();
+        setTimeout(attemptPlace, 10000);
+    }
 }
 
 function place(x, y, color) {
@@ -232,7 +270,7 @@ function place(x, y, color) {
                             'y': y % 1000
                         },
                         'colorIndex': color,
-                        'canvasIndex': (x > 999 ? 1 : 0)
+                        'canvasIndex': getCanvas(x, y)
                     }
                 }
             },
@@ -246,6 +284,14 @@ function place(x, y, color) {
             'Content-Type': 'application/json'
         }
     });
+}
+
+function getCanvas(x, y) {
+    if (x <= 999) {
+        return y <= 999 ? 0 : 2;
+    } else {
+        return y <= 999 ? 1 : 3;
+    }
 }
 
 async function getAccessToken() {
@@ -304,20 +350,34 @@ async function getCurrentImageUrl(id = '0') {
     });
 }
 
-function getCanvasFromUrl(url, canvas, x = 0, y = 0) {
+function getCanvasFromUrl(url, canvas, x = 0, y = 0, clearCanvas = false) {
     return new Promise((resolve, reject) => {
-        var ctx = canvas.getContext('2d');
-        var img = new Image();
-        img.crossOrigin = 'anonymous';
-        img.onload = () => {
-            ctx.drawImage(img, x, y);
-            resolve(ctx);
+        let loadImage = ctx => {
+            var img = new Image();
+            img.crossOrigin = 'anonymous';
+            img.onload = () => {
+                if (clearCanvas) {
+                    ctx.clearRect(0, 0, canvas.width, canvas.height);
+                }
+                ctx.drawImage(img, x, y);
+                resolve(ctx);
+            };
+            img.onerror = () => {
+                Toastify({
+                    text: 'Error retrieving map. Trying again in 3 sec...',
+                    duration: 3000
+                }).showToast();
+                setTimeout(() => loadImage(ctx), 3000);
+            };
+            img.src = url;
         };
-        img.onerror = reject;
-        img.src = url;
+        loadImage(canvas.getContext('2d'));
     });
 }
 
 function rgbToHex(r, g, b) {
     return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1).toUpperCase();
 }
+
+let rgbaOrderToHex = (i, rgbaOrder) =>
+    rgbToHex(rgbaOrder[i * 4], rgbaOrder[i * 4 + 1], rgbaOrder[i * 4 + 2]);
